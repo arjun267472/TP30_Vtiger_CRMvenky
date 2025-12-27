fe55854884a4215ce648d694f4d842f40daf7b69
NAGARAJ <NAGARAJ@DESKTOP-IODHK5L>
2025-12-27 12:55:45 +0530

    Merge branch 'Tesk01' of https://github.com/venkateshnm-5335/TP30_Vtiger_CRM.git into Tesk01

---
+++ src/test/java/testing/SimpleTest.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class SimpleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Venkatesh-- Good morning");
+	}
+
+}
